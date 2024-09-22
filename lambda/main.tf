b1ad10f1694c003d19498962266ee5c23b0bffb5
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-09-22 10:13:45 +0100

    initial deploy12

--- lambda/main.tf
+++ lambda/main.tf
@@ -45,10 +45,10 @@ resource "aws_lambda_function" "aurora_watch" {
   }
 }
 
-import {
-  id = "arn:aws:iam::${var.aws_account_id}:role/aurora_watch_lambda_role"  # Use the variable for the account ID
-  to = aws_iam_role.lambda_exec
-}
+# import {
+#  id = "arn:aws:iam::${var.aws_account_id}:role/aurora_watch_lambda_role"  # Use the variable for the account ID
+#  to = aws_iam_role.lambda_exec
+# }
 
 resource "aws_iam_role" "lambda_exec" {
   name = "aurora_watch_lambda_role"
@@ -67,28 +67,28 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.lambda_exec.name
-}
+#resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+#  role       = aws_iam_role.lambda_exec.name
+#}
 
-resource "aws_cloudwatch_event_rule" "every_six_hours" {
-  name                = "every-six-hours"
-  description         = "Fires every six hours"
-  schedule_expression = "rate(6 hours)"
-}
+#resource "aws_cloudwatch_event_rule" "every_six_hours" {
+#  name                = "every-six-hours"
+#  description         = "Fires every six hours"
+#  schedule_expression = "rate(6 hours)"
+#}
 
-resource "aws_cloudwatch_event_target" "run_lambda_every_six_hours" {
-  rule      = aws_cloudwatch_event_rule.every_six_hours.name
-  target_id = "aurora_watch_lambda"
-  arn       = aws_lambda_function.aurora_watch.arn
-}
+#resource "aws_cloudwatch_event_target" "run_lambda_every_six_hours" {
+#  rule      = aws_cloudwatch_event_rule.every_six_hours.name
+#  target_id = "aurora_watch_lambda"
+#  arn       = aws_lambda_function.aurora_watch.arn
+#}
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.aurora_watch.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.every_six_hours.arn
-}
+#resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+#  statement_id  = "AllowExecutionFromCloudWatch"
+#  action        = "lambda:InvokeFunction"
+#  function_name = aws_lambda_function.aurora_watch.function_name
+#  principal     = "events.amazonaws.com"
+#  source_arn    = aws_cloudwatch_event_rule.every_six_hours.arn
+#}
 
