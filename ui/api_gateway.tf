8e1b0359e76e86ca985fe523719f6e7e3bb6cb72
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-11-16 16:52:30 +0000

    adding auth flows for api gateway 13

--- ui/api_gateway.tf
+++ ui/api_gateway.tf
@@ -27,6 +27,15 @@ resource "aws_api_gateway_authorizer" "cognito" {
   provider_arns = [aws_cognito_user_pool.main.arn]
 }
 
+resource "aws_api_gateway_integration" "get_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.my_resource.id
+  http_method             = aws_api_gateway_method.get_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:lambda:<MYREGION>:<MYACCOUNT>:function:<MYFUNCTION>"
+}
+
 # API Gateway Deployment
 resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
