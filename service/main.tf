4888a7cfb340cc8434f877434d7a09b94679eef1
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-11-16 21:17:11 +0000

    adding auth flows for api gateway 19

--- service/main.tf
+++ service/main.tf
@@ -50,57 +50,10 @@ resource "aws_lambda_function" "service_lambda" {
   }
 }
 
-# Create API Gateway
-resource "aws_api_gateway_rest_api" "api" {
-  name = "graphql-api"
-}
 
-# Create API Gateway resource
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "graphql"
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.api.id
-}
 
-# Create API Gateway method
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
-  authorization = "NONE"  # Changed to NONE for testing
-}
 
-# Create API Gateway integration
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.service_lambda.invoke_arn
-}
 
-# Create API Gateway deployment
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.integration]
-
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
-}
-
-# Grant API Gateway permission to invoke Lambda
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.service_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-# Output the API Gateway URL
-output "api_url" {
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}/${aws_api_gateway_resource.resource.path_part}"
-}
 
 # Attach DynamoDB read policy to the Lambda role
 resource "aws_iam_role_policy_attachment" "lambda_dynamodb_read" {
