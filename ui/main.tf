5c0d897a38d3bd4e3e5f6fea23fff48de3812ba9
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-11-16 16:37:56 +0000

    adding auth flows for api gateway 11

--- ui/main.tf
+++ ui/main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+
 
 variable "region" {
   description = "The AWS region to deploy resources"
