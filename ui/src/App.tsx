daeaf11271a6709bf5ce9e20e07ffe48d3a68e30
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-11-23 21:45:07 +0000

    adding graphing 10

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
 import AuthComponent from './AuthComponent';
-
-Amplify.configure(awsconfig);
 
 const App: React.FC = () => {
   return (
