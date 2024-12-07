c4b33f2e79a7ab4b3a46cbb5475a04f6c1c1d6ea
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2024-12-07 15:47:45 +0000

    correcting lack of config values 02

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 interface Config {
   region: string;
   userPoolId: string;
@@ -7,11 +11,11 @@ interface Config {
 }
 
 const config: Config = {
-  region: import.meta.env.VITE_REGION || 'eu-west-2',
-  userPoolId: import.meta.env.VITE_USER_POOL_ID || '',
-  userPoolWebClientId: import.meta.env.VITE_USER_POOL_WEB_CLIENT_ID || '',
-  identityPoolId: import.meta.env.VITE_IDENTITY_POOL_ID || '',
-  cloudfrontUrl: import.meta.env.VITE_CLOUDFRONT_URL || '',
+  region: process.env.VITE_REGION || 'eu-west-2',
+  userPoolId: process.env.VITE_USER_POOL_ID || '',
+  userPoolWebClientId: process.env.VITE_USER_POOL_WEB_CLIENT_ID || '',
+  identityPoolId: process.env.VITE_IDENTITY_POOL_ID || '',
+  cloudfrontUrl: process.env.VITE_CLOUDFRONT_URL || '',
 };
 
 export default config; 
